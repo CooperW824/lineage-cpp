6da03e198a1680f532c929ca2a7ea01004b41fb4
Cooper Werner <cooperwerner824@gmail.com>
2023-12-08 12:38:40 -0500

    Defining Complex Numbers

---
+++ lineage/complex_number.h
@@ -0,0 +1,59 @@
+#ifndef __COMPLEX_NUMBER_H
+#define __COMPLEX_NUMBER_H
+
+namespace lineage
+{
+	typedef struct complex_number
+	{
+		double real;
+		double imaginary;
+
+		complex_number() : real(0.0), imaginary(0.0){};
+		complex_number(double real, double imaginary) : real(real), imaginary(imaginary){};
+		complex_number(const complex_number &other) : real(other.real), imaginary(other.imaginary){};
+		complex_number(double real) : real(real), imaginary(0.0){};
+
+		complex_number &operator=(const complex_number other);
+		complex_number &operator=(double real);
+
+		complex_number operator+(const complex_number &other) const;
+		complex_number operator+(double value) const;
+		complex_number operator-(const complex_number &other) const;
+		complex_number operator-(double value) const;
+
+		complex_number operator*(const complex_number &other) const;
+		complex_number operator*(double factor) const;
+		complex_number operator/(const complex_number &other) const;
+		complex_number operator/(double divisor) const;
+
+		complex_number &operator+=(const complex_number &other) const;
+		complex_number &operator+=(double value) const;
+		complex_number &operator-=(const complex_number &other) const;
+		complex_number &operator-=(double value) const;
+
+		complex_number &operator*=(const complex_number &other) const;
+		complex_number &operator*=(double factor) const;
+		complex_number &operator/=(const complex_number &other) const;
+		complex_number &operator/=(double divisor) const;
+
+		bool operator==(const complex_number &other) const;
+		bool operator==(double value) const;
+		bool operator!=(const complex_number &other) const;
+		bool operator!=(double value) const;
+		bool operator<(const complex_number &other) const;
+		bool operator<(double value) const;
+		bool operator<=(const complex_number &other) const;
+		bool operator<=(double value) const;
+		bool operator>(const complex_number &other) const;
+		bool operator>(double value) const;
+		bool operator>=(const complex_number &other) const;
+		bool operator>=(double value) const;
+		
+		complex_number &operator-() const;
+
+		complex_number &conjugate() const;
+		complex_number to_conjugate() const;
+	};
+}
+
+#endif // __COMPLEX_NUMBER_H
